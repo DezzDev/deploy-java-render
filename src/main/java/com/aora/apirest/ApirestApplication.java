2382e9826e1d04c87623a3ae0a72a92cd41e5e54
Daniel Zapata <daniel-dezz@hotmail.com>
2024-12-05 15:09:13 +0100

    manejamos la ausencia del archivo .env

--- src/main/java/com/aora/apirest/ApirestApplication.java
+++ src/main/java/com/aora/apirest/ApirestApplication.java
@@ -11,13 +11,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ApirestApplication {
 
 	public static void main(String[] args) {
-    
-    // Carga el archivo .env
-		Dotenv dotenv = Dotenv.configure().load();
-    
-		// Opcional: Establece variables de entorno en el sistema (útil para `application.properties`)
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-    
+    try {
+      // Carga el archivo .env
+      Dotenv dotenv = Dotenv.configure().load();
+      
+      // Opcional: Establece variables de entorno en el sistema (útil para `application.properties`)
+      dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+      
+      
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
     
 		SpringApplication.run(ApirestApplication.class, args);
 	
