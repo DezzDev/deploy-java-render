3a6f063202f8351bfc86ec2661fbf48e8d996435
Daniel Zapata <daniel-dezz@hotmail.com>
2024-12-10 13:06:18 +0100

    add cors

--- src/main/java/com/aora/apirest/config/SecurityConfig.java
+++ src/main/java/com/aora/apirest/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.aora.apirest.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.aora.apirest.jwt.JwtAuthenticationFilter;
 
@@ -33,6 +38,7 @@ public class SecurityConfig {
     // toda ruta que empiece por /auth se permite
     return http
       .csrf(csrf -> csrf.disable()) // deshabilitar csrf
+      .cors(cors -> cors.configurationSource(corsConfigurationSource()))
       .authorizeHttpRequests(authRequest ->
         authRequest
           .requestMatchers("/auth/**").permitAll() // permite todas las rutas que empiezan por /auth
@@ -43,5 +49,16 @@ public class SecurityConfig {
       .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
       .build();
   }
- 
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*")); // permite todas las origenes
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("*")); // permite todos los headers
+    configuration.setExposedHeaders(Arrays.asList("Authorization")); // expone el header Authorization
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration); // aplica la configuración a todas las rutas
+    return source;
+  }
 }
